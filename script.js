2557061e16f34652c0582abd378defaf5034dc43
Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>
2023-08-22 18:43:05 +0300

    Added support for dark mode

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+// Icons for themes
+const default_icon = `<svg class="MuiSvgIcon-root" width="25" height="25" fill="currentColor" id="theme-toggler" onclick="toggleTheme()" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-.89 0-1.74-.2-2.5-.55C11.56 16.5 13 14.42 13 12s-1.44-4.5-3.5-5.45C10.26 6.2 11.11 6 12 6c3.31 0 6 2.69 6 6s-2.69 6-6 6z"></path></svg>`;
+const dark_icon = `<svg class="MuiSvgIcon-root" width="25" height="25" fill="currentColor" id="theme-toggler" onclick="toggleTheme()" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6zm0-10c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"></path></svg>`;
+
+// Element for theme toggle
+const togglerdiv = document.querySelector('#toggler-div');
+
+// Function to set the theme icon and data-bs-theme attribute based on user preference or stored theme
+const checkTheme = () => {
+    try {
+        if (localStorage.getItem('website_theme') === 'dark_mode') {
+            togglerdiv.innerHTML = dark_icon;
+            document.documentElement.setAttribute('data-bs-theme', 'dark');
+        } else {
+            togglerdiv.innerHTML = default_icon;
+            document.documentElement.setAttribute('data-bs-theme', 'light');
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+// Call the function to set initial theme icon and data-bs-theme attribute
+checkTheme();
+
+// Function for toggling theme
+const toggleTheme = () => {
+    try {
+        if (localStorage.getItem('website_theme') === 'dark_mode') {
+            localStorage.setItem('website_theme', 'default');
+        } else {
+            localStorage.setItem('website_theme', 'dark_mode');
+        }
+        checkTheme(); // Update the theme icon and data-bs-theme attribute
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+// Check if theme is set and set theme based on stored preference
+const getTheme = () => {
+    try {
+        let theme = localStorage.getItem('website_theme');
+        if (theme != null) {
+            checkTheme(); // Update the theme icon and data-bs-theme attribute
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+getTheme();
+
 // Function to fetch and display the food menu
 async function fetchFoodMenu(URL, divId) {
     try {
@@ -9,11 +61,10 @@ async function fetchFoodMenu(URL, divId) {
             },
         });
 
-        console.log('Fetching data...');
         document.getElementById(divId).innerHTML = '<p>Fetching data...</p>';
 
         const data = await response.json();
-        console.log('Data fetched:', data);
+        console.log('Fetched data:', data);
 
         // Clear the div
         document.getElementById(divId).innerHTML = '';
