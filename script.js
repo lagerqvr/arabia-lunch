d34126fdd0a53bd48bae653caed4ceea682087b7
Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>
2023-10-21 19:42:03 +0300

    Changed date setting for localStorage

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const default_icon = `<svg class="MuiSvgIcon-root" width="25" height="25" fill="currentColor" id="theme-toggler" onclick="toggleTheme()" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-.89 0-1.74-.2-2.5-.55C11.56 16.5 13 14.42 13 12s-1.44-4.5-3.5-5.45C10.26 6.2 11.11 6 12 6c3.31 0 6 2.69 6 6s-2.69 6-6 6z"></path></svg>`;
 const dark_icon = `<svg class="MuiSvgIcon-root" width="25" height="25" fill="currentColor" id="theme-toggler" onclick="toggleTheme()" viewBox="0 0 24 24" aria-hidden="true"><path d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6zm0-10c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"></path></svg>`;
 
+// Set the current date in localStorage
+const date = new Date();
+localStorage.setItem('currentDate', date.toISOString());
+
 // Element for theme toggle
 const togglerdiv = document.querySelector('#toggler-div');
 
@@ -10,8 +14,7 @@ let chosenLang = localStorage.getItem('selected-language');
 if (chosenLang === null) {
     chosenLang = 'en';
     localStorage.setItem('selected-language', 'en');
-}
-
+};
 
 // JavaScript function to set language
 document.addEventListener('DOMContentLoaded', () => {
